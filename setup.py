fdb9d817833cd9770c2eca9475f736ce8519fda8
Alguem <iagobr131313@gmail.com>
2019-05-14 22:34:17 -0300

    Version 1.2.0a
    Update version, and import improvements

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
-import setuptools
+from setuptools import setup, find_packages
 
-setuptools.setup(name='demojipy',
-                 version='1.0',
-                 license='MIT',
-                 description='Python API Wrapper for DiscordEmoji',
-                 author='Algueem',
-                 url='https://github.com/Algueem/DiscordEmojiPy',
-                 keywords=['pydemoji', 'discordemoji'],
-                 install_requires=[
-                     'requests',
-                     'json'
-                 ],
-                 packages=setuptools.find_packages()
-                 )
+setup(name='demojipy',
+      version='1.2.0a',
+      license='MIT',
+      description='Python API Wrapper for DiscordEmoji',
+      author='Algueem',
+      url='https://github.com/Algueem/DiscordEmojiPy',
+      keywords=['pydemoji', 'discordemoji'],
+      install_requires=[
+          'requests'
+      ],
+      packages=find_packages(),
+      classifiers=[
+          "Development Status :: 3 - Alpha",
+          "Programming Language :: Python :: 3.6",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent"
+      ]
+      )
