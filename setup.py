59f0b0074d14be2cdf9eaec473ac94b7094d6d57
Alguem <iagobr131313@gmail.com>
2019-05-15 11:47:39 -0300

    Version 1.3.0
    Added long description

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup, find_packages
 
+
+with open('README.md') as fp:
+    long_desc = fp.read()
+
+
 setup(name='demojipy',
-      version='1.2.0a',
+      version='1.3.0',
       license='MIT',
-      description='Python API Wrapper for DiscordEmoji',
+      description='Python Wrapper for DiscordEmoji API',
+      long_description=long_desc,
       author='Algueem',
       url='https://github.com/Algueem/DiscordEmojiPy',
       keywords=['pydemoji', 'discordemoji'],
@@ -12,7 +18,7 @@ setup(name='demojipy',
       ],
       packages=find_packages(),
       classifiers=[
-          "Development Status :: 3 - Alpha",
+          "Development Status :: 5 - Production/Stable",
           "Programming Language :: Python :: 3.6",
           "License :: OSI Approved :: MIT License",
           "Operating System :: OS Independent"
